83f0751ae65bba91f6adf085eae9be797a7f575a
py444 <44gdev@gmail.com>
2016-10-24 20:46:39 +0200

    Add delete task option

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,7 @@
-var now = new Date();
-$('#task-add').on('click', function () {
-  if ($('#task-input').val() == "") return 0;
-
-  var appHeigth = parseInt($('.main-app').css("height")) + 40;
-  $('.main-app').height(parseInt(appHeigth));
-
-  var task = $('.not-checked').first().clone(true, true);
-  var taskText = $('#task-input').val();
-  task.text(capitalizeFirstLetter(taskText));
-  var time = now.getHours() + ":" + now.getMinutes();
-  task.attr('time', time);
-
-  task.prop('class', 'not-checked');
-  $('.tasks').append(task);
-
-  updateTaskPending();
-  $('#task-input').val('');
-})
-
-$('li').on('click', function() {
-  $(this).prop('class', 'checked');
-  updateTaskPending();
-  $(this).fadeTo( 'slow', 0.5, function () {
-    $(this).css({ "text-decoration" : "line-through"});
-  });
-});
-
 $(document).ready(function () {
-  $('.not-checked').attr('time','7:00');
+  $('.task-time').text('07:00');
+  $('.task-time').append($('<span></span>').prop('class', 'task-delete icon ion-android-delete'));
+
   updateTaskPending();
 
   var day = now.getWeekDay();
@@ -41,6 +15,45 @@ function updateTaskPending() {
   var taskCount = $('.not-checked').length - 1;
   $('#task').text(taskCount + " tasks pending");
 }
+
+var now = new Date();
+$('#task-add').on('click', function () {
+  if ($('#task-input').val() == "") return;
+
+  var appHeigth = parseInt($('.main-app').css("height")) + 40;
+  $('.main-app').height(parseInt(appHeigth));
+
+  var task = $('.not-checked').first().clone(true, true);
+  var taskText = $('#task-input').val();
+  task.text(capitalizeFirstLetter(taskText));
+
+  var timeSpan = $('<span></span>').prop('class', 'task-time');
+  var time = now.getHours() + ":" + now.getMinutes();
+  timeSpan.text(time);
+  timeSpan.append($('<span></span>').text("X").prop('class', 'task-delete'));
+  task.append(timeSpan);
+
+  task.prop('class', 'not-checked');
+  $('.tasks').append(task);
+
+  updateTaskPending();
+  $('#task-input').val('');
+})
+
+$('li').on('click', function(e) {
+  if (!$(e.target).is($('.task-delete'))) {
+    $(this).prop('class', 'checked');
+    updateTaskPending();
+    $(this).fadeTo( 'slow', 0.5, function() {
+      $(this).css({ "text-decoration" : "line-through"});
+    })
+  }else {
+  $(this).remove(); // delete current task
+  updateTaskPending()
+  var appHeigth = parseInt($('.main-app').css("height")) - 40;
+  $('.main-app').height(parseInt(appHeigth));
+  }
+})
 
 function capitalizeFirstLetter(text) {
   return text.substring(0,1).toUpperCase() + text.substring(1,text.length).toLowerCase();
